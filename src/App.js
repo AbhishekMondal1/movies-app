3b1ea6cbbfda8945b45a2eced8288985f6f67731
AbhishekMondal1 <abhishekmondal.official@gmail.com>
2020-09-28 13:12:21 +0530

    movies component added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Movie from './components/Movies';
+
+function App() {
+    const movies = ['1', '2', '3'];
+
+    return <div>
+        {movies.map(movie => (
+            <Movie/>
+        ))}
+         </div>
+}
+
+export default App;
---
+++ src/components/Movies.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Movie = () => <div 
+className="movie">
+    This is a movie Component.
+    </div>;
+
+export default Movie;
